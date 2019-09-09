cd559775cedc4ce25801d72d8c3ef1ed4878d5fc
elitk <44366381+elitk@users.noreply.github.com>
2019-09-09 18:26:21 +0300

    first commit

--- src/components/Square/Square.jsx
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react';
-function Square (props){
-    return(
-        <button className="square" onClick={props.onClick}>
-            {props.value}
-        </button>
-    );
-}
-export default Square;
